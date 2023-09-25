949242ba33b2a74dc5bad45281b738bce98a5b04
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2023-09-24 23:00:08 -0600

    Oops

--- data/mods/m4akalos/abilities.ts
+++ data/mods/m4akalos/abilities.ts
@@ -185,12 +185,12 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 				attacker.addVolatile('hyperspacemayhem', attacker); // appropriately modify certain moves, like Teleport and Shadow Force
 				attacker.formeChange(this.dex.species.get(summon), move); // make sure this is silent?
 				this.add('-message', `It's ${attacker.name}!`);
-				if (hyperspaceLookup[randomPick].move === "Geomancy" || hyperspaceLookup[randomPick].move === "Shadow Force") {
-					this.add('-prepare', attacker, hyperspaceLookup[randomPick]);
+				if (hyperspaceLookup[summon].move === "Geomancy" || hyperspaceLookup[summon].move === "Shadow Force") {
+					this.add('-prepare', attacker, hyperspaceLookup[summon].move);
 					attacker.addVolatile('twoturnmove', defender);
 				}
-				this.useMove(hyperspaceLookup[randomPick].move, attacker); // use the move
-				if (hyperspaceLookup[randomPick].move === "Teleport") this.add('-message', `Oops! Looks like ${attacker.name} doesn't know how to battle!`);
+				this.useMove(hyperspaceLookup[summon].move, attacker); // use the move
+				if (hyperspaceLookup[summon].move === "Teleport") this.add('-message', `Oops! Looks like ${attacker.name} doesn't know how to battle!`);
 				if (attacker.volatiles['mustrecharge']) delete attacker.volatiles['mustrecharge']; // for Dialga
 
 				// to do: make a special exception for Zacian and Rayquaza's stat modifiers
