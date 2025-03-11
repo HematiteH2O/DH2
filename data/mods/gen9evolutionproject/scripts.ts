001deacdd60313264584482b4db9418f6c33075b
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2025-03-11 01:33:42 -0600

    I'm really stupid

--- data/mods/gen9evolutionproject/scripts.ts
+++ data/mods/gen9evolutionproject/scripts.ts
@@ -6,7 +6,7 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 		customDoublesTiers: ['Pokémon of the Day!', 'S1P1', 'S1P2', 'S1P3', 'Evo 1', 'Canon', 'Evo (NFE)'],
 	},
 	init() {
-		let megaList = [];
+		let customList = [];
 		for (const id in this.dataCache.Pokedex) {
 			const notm = ['terablast', 'hiddenpower']; // certain moves don't count TMs
 			const gen9only = [
