64bb1ba2d570b8ea7657c71f0b8beaafbf9323ac
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2025-03-14 23:56:44 -0600

    Update scripts.ts

--- data/mods/gen9evolutionproject/scripts.ts
+++ data/mods/gen9evolutionproject/scripts.ts
@@ -77,4 +77,106 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 		this.modData('FormatsData', customList[random2]).tier = "Pokémon of the Day!";
 		this.modData('FormatsData', customList[random3]).tier = "Pokémon of the Day!";
 	},
+	teams: {
+		parseExportedTeamLine(line: string, isFirstLine: boolean, set: PokemonSet, aggressive?: boolean) {
+			if (isFirstLine) {
+				let item;
+				[line, item] = line.split(' @ ');
+				if (item) {
+					set.item = item;
+					if (toID(set.item) === 'noitem') set.item = '';
+				}
+				if (line.endsWith(' (M)')) {
+					set.gender = 'M';
+					line = line.slice(0, -4);
+				}
+				if (line.endsWith(' (F)')) {
+					set.gender = 'F';
+					line = line.slice(0, -4);
+				}
+				if (line.endsWith(')') && line.includes('(')) {
+					const [name, species] = line.slice(0, -1).split('(');
+					set.species = Dex.species.get(species).name;
+					set.name = name.trim();
+				} else {
+					set.species = Dex.species.get(line).name;
+					set.name = '';
+				}
+			} else if (line.startsWith('Trait: ')) {
+				line = line.slice(7);
+				set.ability = aggressive ? toID(line) : line;
+			} else if (line.startsWith('Ability: ')) {
+				line = line.slice(9);
+				set.ability = aggressive ? toID(line) : line;
+			} else if (line === 'Shiny: Yes') {
+				set.shiny = true;
+			} else if (line.startsWith('Level: ')) {
+				line = line.slice(7);
+				set.level = +line;
+			} else if (line.startsWith('Happiness: ')) {
+				line = line.slice(11);
+				set.happiness = +line;
+			} else if (line.startsWith('Pokeball: ')) {
+				line = line.slice(10);
+				set.pokeball = aggressive ? toID(line) : line;
+			} else if (line.startsWith('Hidden Power: ')) {
+				line = line.slice(14);
+				set.hpType = aggressive ? toID(line) : line;
+			} else if (line.startsWith('Tera Type: ')) {
+				line = line.slice(11);
+				set.teraType = aggressive ? line.replace(/[^a-zA-Z0-9]/g, '') : line;
+			} else if (line === 'Gigantamax: Yes') {
+				set.gigantamax = true;
+			} else if (line.startsWith('EVs: ')) {
+				line = line.slice(5);
+				const evLines = line.split('/');
+				set.evs = {hp: 0, atk: 0, def: 0, spa: 0, spd: 0, spe: 0};
+				for (const evLine of evLines) {
+					const [statValue, statName] = evLine.trim().split(' ');
+					const statid = Dex.stats.getID(statName);
+					if (!statid) continue;
+					const value = parseInt(statValue);
+					set.evs[statid] = value;
+				}
+			} else if (line.startsWith('IVs: ')) {
+				line = line.slice(5);
+				const ivLines = line.split('/');
+				set.ivs = {hp: 31, atk: 31, def: 31, spa: 31, spd: 31, spe: 31};
+				for (const ivLine of ivLines) {
+					const [statValue, statName] = ivLine.trim().split(' ');
+					const statid = Dex.stats.getID(statName);
+					if (!statid) continue;
+					let value = parseInt(statValue);
+					if (isNaN(value)) value = 31;
+					set.ivs[statid] = value;
+				}
+			} else if (line.startsWith('Tagline: ')) {
+				line = line.slice(9);
+				set.tagline = line.trim();
+			} else if (/^[A-Za-z]+ (N|n)ature/.test(line)) {
+				let natureIndex = line.indexOf(' Nature');
+				if (natureIndex === -1) natureIndex = line.indexOf(' nature');
+				if (natureIndex === -1) return;
+				line = line.substr(0, natureIndex);
+				if (line !== 'undefined') set.nature = aggressive ? toID(line) : line;
+			} else if (line.startsWith('-') || line.startsWith('~')) {
+				line = line.slice(line.charAt(1) === ' ' ? 2 : 1);
+				if (line.startsWith('Hidden Power [')) {
+					const hpType = line.slice(14, -1);
+					line = 'Hidden Power ' + hpType;
+					if (!set.ivs && Dex.types.isName(hpType)) {
+						set.ivs = {hp: 31, atk: 31, def: 31, spa: 31, spd: 31, spe: 31};
+						const hpIVs = Dex.types.get(hpType).HPivs || {};
+						for (const statid in hpIVs) {
+							set.ivs[statid as StatID] = hpIVs[statid as StatID]!;
+						}
+					}
+				}
+				if (line === 'Frustration' && set.happiness === undefined) {
+					set.happiness = 0;
+				}
+				set.moves.push(line);
+			}
+		}
+	},
 };
