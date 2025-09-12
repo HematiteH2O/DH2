6af8e012a4001d481dcdcd1ef10f8c955606ed6d
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2025-09-11 21:24:05 -0600

    there's no way this all works is there

--- data/mods/gen9evolutionproject/moves.ts
+++ data/mods/gen9evolutionproject/moves.ts
@@ -385,49 +385,339 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 
 	// Slate 2
 
-/*
-Wallow			Water		Physical	50	100	10
-The higher the user's Sp. Def stat, the greater the damage this move deals.
-Hitting a target raises the Sp. Def stat.
-
-Prance and Pierce			Ghost		Physical	100	100	10
-Before attacking, the user switches places with a party Pokémon in waiting, if any.
-Two turns after this move is used, it finally returns to the field to attack a random active opponent.
-This also poisons the target.
-(biting)
-
-Grand Finale			Fire		Physical	25	90	10
-A consecutive three-strike attack that becomes more powerful with each successful hit. (25 -> 50 -> 75)
-After using this move, the user will no longer be Fire type. (Fails if the user is not Fire-type.)​
-If it lands all three hits, the user rushes back to switch places with a party Pokémon in waiting.								
-
-Slime Cannon			Poison		Special	110	100	5
-A slimy, somewhat befuddling all-out attack.​
-When the user knocks out a target with this move, all remaining Pokémon adjacent to the target have their Attack and Sp. Atk lowered (including the user and its allies).​
-(blocked by Bulletproof)
-
-Total Eclipse			Dark		Status	--	--	10
-The user steals its ally's stat changes.
-For the rest of the turn, any damage taken by the ally is instead taken by the user.
-This move can't be used twice in a row.
-
-Flowing Flare			Fire		Special	--	100	10
-This move inflicts damage using the Attack or Sp. Atk stat - whichever is higher for the user.
-The faster the user is than the target, the greater the move's power. (scales the same as Electro Ball, except that the max BP is 180 instead of 150)
-
-Paranoia			Bug		Special	--	95	10
-An attack that reduces each target's current HP by a quarter.
-This also lowers each target's most proficient stat.
-(spread)
-
-Psykick			Psychic		Physical	70	100	15
-This also leaves the target incapable of using the same move twice in a row.								
-
-Empathic Pulse			Psychic		Status	--	100	5
-The user transfers its status conditions to adjacent opponents.
-This also raises its most proficient stat.
-(spread)
-*/
+	wallow: {
+		num: -13,
+		accuracy: 100,
+		basePower: 50,
+		category: "Physical",
+		name: "Wallow",
+		pp: 10,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1, metronome: 1},
+		overrideOffensiveStat: 'spd',
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Mud Sport", source);
+			this.add('-anim', source, "Liquidation", target);
+		},
+		secondary: {
+			chance: 100,
+			self: {
+				boosts: {
+					spd: 1,
+				},
+			},
+		},
+		target: "normal",
+		type: "Water",
+		shortDesc: "Damage based on Sp. Def. Boosts Sp. Def after use.",
+	},
+	pranceandpierce: {
+		num: -14,
+		accuracy: 100,
+		basePower: 100,
+		category: "Physical",
+		name: "Prance and Pierce",
+		pp: 10,
+		priority: 0,
+		flags: {contact: 1, bite: 1, allyanim: 1, metronome: 1, futuremove: 1},
+		onTryHit(source) {
+			if (!this.canSwitch(source.side) || source.volatiles['commanded']) {
+				this.attrLastMove('[still]');
+				this.add('-fail', source);
+				return this.NOT_FAIL;
+			}
+		},
+		selfSwitch: true,
+		ignoreImmunity: true,
+		onTry(source, target) {
+			if (!target.side.addSlotCondition(target, 'futuremove')) return false;
+			Object.assign(target.side.slotConditions[target.position]['futuremove'], {
+				duration: 3,
+				move: 'futuresight',
+				source: source,
+				sourcePosition: source.position,
+				moveData: {
+					id: 'pranceandpierce',
+					name: "Prance and Pierce",
+					accuracy: 100,
+					basePower: 100,
+					category: "Physical",
+					priority: 0,
+					flags: {contact: 1, bite: 1, allyanim: 1, metronome: 1, futuremove: 1},
+					onTryHit(source, move) {
+						// if the source isn't on the field...
+						if (source && !source.isActive && source.hp && this.canSwitch(source.Side)) {
+							switchIn(source, move.sourcePosition);
+						}
+					},
+					selfSwitch: false,
+					ignoreImmunity: false,
+					effectType: 'Move',
+					secondary: {
+						chance: 100,
+						status: 'psn',
+					},
+					type: 'Ghost',
+				},
+			});
+			this.add('-start', source, 'move: Prance and Pierce');
+			return this.NOT_FAIL;
+		},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Bounce", source);
+			this.add('-anim', source, "Super Fang", target);
+		},
+		secondary: {
+			chance: 100,
+			status: 'psn',
+		},
+		target: "normal",
+		type: "Ghost",
+		contestType: "Clever",
+		shortDesc: "User pivots out, then comes back in to attack two turns later. Poison target.",
+	},
+	grandfinale: {
+		num: -15,
+		accuracy: 90,
+		basePower: 25,
+		basePowerCallback(pokemon, target, move) {
+			if (move.hit === 3) move.selfSwitch = true; // I hope this works
+			return 25 * move.hit;
+		},
+		onTryMove(pokemon, target, move) {
+			if (pokemon.hasType('Fire')) return;
+			this.add('-fail', pokemon, 'move: Grand Finale');
+			this.attrLastMove('[still]');
+			return null;
+		},
+		onAfterMove(pokemon) {
+			pokemon.setType(pokemon.getTypes(true).map(type => type === "Fire" ? "???" : type));
+			this.add('-start', pokemon, 'typechange', pokemon.getTypes().join('/'), '[from] move: Burn Up'); // I think this still needs Burn Up's message
+		},
+		category: "Physical",
+		name: "Grand Finale",
+		pp: 10,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1, metronome: 1},
+		multihit: 3,
+		multiaccuracy: true,
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Wish", source);
+			this.add('-anim', source, "Mind Blown", target);
+		},
+		secondary: null,
+		target: "normal",
+		type: "Fire",
+		zMove: {basePower: 120},
+		maxMove: {basePower: 140},
+		shortDesc: "Hits up to 3 times (25 -> 50 -> 75). Removes Fire type; if hits all 3, pivots out.",
+	},
+	slimecannon: {
+		num: -16,
+		accuracy: 100,
+		basePower: 110,
+		category: "Special",
+		name: "Slime Cannon",
+		pp: 5,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, metronome: 1, bullet: 1},
+		onAfterMoveSecondarySelf(pokemon, target, move) {
+			if (!target || target.fainted || target.hp <= 0) {
+				const targets: Pokemon[] = [];
+				for (const valid of this.getAllActive()) {
+					if (valid.isAdjacent(target)) {
+						// This move affects every grounded Grass-type Pokemon in play.
+						targets.push(pokemon);
+					}
+				}
+				if (!targets.length) return;
+				if (targets.length > 1) {
+					 this.add('-message', `Everyone was covered in slime!`);
+				}
+				for (const debuff of targets) {
+					if (targets.length === 1) this.add('-message', `${debuff.illusion ? debuff.illusion.name : debuff.name} was covered in slime!`);
+					this.boost({atk: -1, spa: -1}, debuff, source);
+				}
+			}
+		},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Acid Downpour", target);
+		},
+		secondary: null,
+		target: "normal",
+		type: "Poison",
+		contestType: "Clever",
+		shortDesc: "If target is KOed, -1 to everyone's Attack and Sp. Atk.",
+	},
+	totaleclipse: {
+		num: -17,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		name: "Total Eclipse",
+		pp: 10,
+		priority: 3,
+		flags: {bypasssub: 1, noassist: 1, failcopycat: 1, allyanim: 1, cantusetwice: 1},
+		onHit(target, source) {
+			let i: BoostID;
+			for (i in target.boosts) {
+				source.boosts[i] = target.boosts[i];
+			}
+			const volatilesToCopy = ['dragoncheer', 'focusenergy', 'gmaxchistrike', 'laserfocus'];
+			for (const volatile of volatilesToCopy) {
+				if (target.volatiles[volatile]) {
+					source.addVolatile(volatile);
+					if (volatile === 'gmaxchistrike') source.volatiles[volatile].layers = target.volatiles[volatile].layers;
+					if (volatile === 'dragoncheer') source.volatiles[volatile].hasDragonType = target.volatiles[volatile].hasDragonType;
+				} else {
+					source.removeVolatile(volatile);
+				}
+			}
+			this.add('-copyboost', source, target, '[from] move: Total Eclipse');
+			target.clearBoosts();
+			this.add('-clearboost', target);
+			this.add('-message', `${source.illusion ? source.illusion.name : source.name} will take damage for ${target.illusion ? target.illusion.name : target.name} this turn!`);
+			target.addVolatile('totaleclipse');
+		},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Black Hole Eclipse", target);
+		},
+		secondary: null,
+		condition: {
+			duration: 1,
+			onDamage(damage, target, source, effect) {
+				if (effect.effectType !== 'Move' && this.effectState.source.isActive && this.effectState.source.hp) {
+					this.add('-message', `${this.effectState.source.illusion ? this.effectState.source.illusion.name : this.effectState.source.name} took damage for ${target.illusion ? target.illusion.name : target.name}!`);
+					this.damage(this.effectState.source(damage), source, target);
+					return false;
+				}
+			},
+		},
+		target: "adjacentAlly",
+		type: "Dark",
+		zMove: {effect: 'heal'},
+		contestType: "Clever",
+		shortDesc: "Steals ally's boosts, then takes damage for the ally. Can't use consecutively.",
+	},
+	flowingflare: {
+		num: -18,
+		accuracy: 100,
+		basePower: 0,
+		basePowerCallback(pokemon, target) {
+			let ratio = Math.floor(pokemon.getStat('spe') / target.getStat('spe'));
+			if (!isFinite(ratio)) ratio = 0;
+			const bp = [40, 60, 80, 120, 180][Math.min(ratio, 4)];
+			this.debug('BP: ' + bp);
+			return bp;
+		},
+		onModifyMove(move, pokemon) {
+			if (pokemon.getStat('atk', false, true) > pokemon.getStat('spa', false, true)) move.category = 'Physical';
+		},
+		category: "Special",
+		name: "Flowing Flare",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, metronome: 1},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Flamethrower", target);
+		},
+		secondary: null,
+		target: "normal",
+		type: "Fire",
+		zMove: {basePower: 160},
+		maxMove: {basePower: 130},
+		contestType: "Cool",
+		shortDesc: "Atk > SpA: physical. Stronger the faster the user is than the target (180 max).",
+	},
+	paranoia: {
+		num: -19,
+		accuracy: 95,
+		basePower: 0,
+		damageCallback(pokemon, target) {
+			return this.clampIntRange(target.getUndynamaxedHP() / 4, 1);
+		},
+		category: "Special",
+		name: "Paranoia",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, metronome: 1},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "First Impression", target);
+			this.add('-anim', source, "Infestation", target);
+		},
+		secondary: {
+			chance: 100,
+			onHit(target, source, move) {
+				const bestStat = target.getBestStat(false, true);
+				this.boost({[bestStat]: -1}, target);
+			},
+		},
+		target: "normal",
+		type: "Bug",
+		contestType: "Clever",
+		shortDesc: "Damage is 1/4 of target's HP. Lowers target's best stat.",
+	},
+	psykick: {
+		num: -20,
+		accuracy: 100,
+		basePower: 70,
+		category: "Physical",
+		name: "Psykick",
+		pp: 15,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1, metronome: 1},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Psychic Noise", target);
+			this.add('-anim', source, "Low Kick", target);
+		},
+		secondary: {
+			chance: 100,
+			volatileStatus: 'torment',
+		},
+		target: "normal",
+		type: "Psychic",
+		contestType: "Clever",
+		shortDesc: "Inflicts Torment.",
+	},
+	empathicpulse: {
+		num: -21,
+		accuracy: 100,
+		basePower: 0,
+		category: "Status",
+		name: "Empathic Pulse",
+		pp: 5,
+		priority: 0,
+		flags: {protect: 1, reflectable: 1, mirror: 1, metronome: 1, pulse: 1},
+		onTryHit(target, source, move) {
+			if (!source.status) return false;
+			move.status = source.status;
+		},
+		self: {
+			onHit(pokemon) {
+				const bestStat = pokemon.getBestStat(false, true);
+				this.boost({[bestStat]: 1}, pokemon);
+				pokemon.cureStatus();
+			},
+		},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Heal Pulse", target);
+		},
+		secondary: null,
+		target: "allAdjacentFoes",
+		type: "Psychic",
+		zMove: {boost: {spa: 2}},
+		contestType: "Clever",
+		shortDesc: "Passes status to adjacent foes, then raises most proficient stat.",
+	},
 
 // modded canon moves
 
