2c498e311c9a9ca986efec10a54d73873e331ead
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2026-03-27 19:46:06 -0600

    Fixed

--- data/mods/gen9evolutionproject/rulesets.ts
+++ data/mods/gen9evolutionproject/rulesets.ts
@@ -22,12 +22,11 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 						showFakemon = true;
 						let abilities = species.abilities[0];
 						if (species.abilities[1]) abilities += ` / ${species.abilities[1]}`;
-						if (species.abilities['H']) abilities += ` / ${species.abilities['H']}`;
-						if (species.abilities['S']) abilities += ` / ${species.abilities['S']}`;
+						if (species.abilities['H']) abilities += ` // ${species.abilities['H']}`;
+						if (species.abilities['S']) abilities += ` // (${species.abilities['S']})`;
 						const baseStats = species.baseStats;
-						const type = species.types[0];
-						hideBox += `<div class="message"><ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="http://play.pokemonshowdown.com/sprites/types/${type}.png" alt="${type}" height="14" width="32">`;
-						if (species.types[1]) hideBox += `<img src="http://play.pokemonshowdown.com/sprites/types/${type2}.png" alt="${type2}" height="14" width="32">`;
+						hideBox += `<div class="message"><ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="http://play.pokemonshowdown.com/sprites/types/${species.types[0]}.png" alt="${species.types[0]}" height="14" width="32">`;
+						if (species.types[1]) hideBox += `<img src="http://play.pokemonshowdown.com/sprites/types/${species.types[1]}.png" alt="${species.types[1]}" height="14" width="32">`;
 						hideBox += `</span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities + `</span><span class="col abilitycol"></span></span><br><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul></div>`;
 						if (species.creator) hideBox += `<div class="hint">${species.name} was created by ${species.creator}!</div>`;
 					}
