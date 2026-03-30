1ba7e98ece3b2b804ecf673cc23912eee543c0a5
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2026-03-29 19:52:12 -0600

    Create team-validator.ts

---
+++ data/mods/gen9evolutionproject/team-validator.ts
@@ -0,0 +1,17 @@
+export class TeamValidator {
+  inherit: true,
+  // just testing: does this affect me or everyone?
+  validateTeam(
+		team: PokemonSet[] | null,
+		options: {
+			removeNicknames?: boolean,
+			skipSets?: {[name: string]: {[key: string]: boolean}},
+		} = {}
+	): string[] | null {
+    return [`This is Carpet's modded thing`];
+		if (team && this.format.validateTeam) {
+			return this.format.validateTeam.call(this, team, options) || null;
+		}
+		return this.baseValidateTeam(team, options);
+	},
+};
