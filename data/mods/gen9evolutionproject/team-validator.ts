4e18f8728ff5516bd48964568e841acef2437c9d
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2026-03-29 19:58:31 -0600

    testing

--- data/mods/gen9evolutionproject/team-validator.ts
+++ data/mods/gen9evolutionproject/team-validator.ts
@@ -1,14 +1,14 @@
-export class TeamValidator = {
-  inherit: true,
-  // just testing: does this affect me or everyone?
-  validateTeam(
+export type TeamValidator: import('../../../sim/team-validator').TeamValidator = {
+	inherit: true,
+	// just testing: does this affect me or everyone?
+	validateTeam(
 		team: PokemonSet[] | null,
 		options: {
 			removeNicknames?: boolean,
 			skipSets?: {[name: string]: {[key: string]: boolean}},
 		} = {}
 	): string[] | null {
-    return [`This is Carpet's modded thing`];
+		return [`This is Carpet's modded thing`];
 		if (team && this.format.validateTeam) {
 			return this.format.validateTeam.call(this, team, options) || null;
 		}
