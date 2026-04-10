5c41ff907d30f338b1eb84016a0ee068558517c0
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2026-04-09 22:54:25 -0600

    wait

--- data/mods/gen9evolutionproject/scripts.ts
+++ data/mods/gen9evolutionproject/scripts.ts
@@ -1745,7 +1745,7 @@ singles ['choicebreaker', 'priority', 'entryhazard', 'hazardcontrol', 'knockoff'
 				// TODO: EVs later
 				// TODO: item clause later
 				
-				if (!fragment.ability && !fragment.item && !fragment.teraType && !fragment.moves && !fragment.evs) {
+				if (!fragment.ability && !fragment.item && !fragment.teraType && !(fragment.moves && fragment.moves.length) && !fragment.evs) {
 					// the fragment is already complete, so I should also check it off of the role tally and then delete it from the fragments list
 					// but I don't have a role tally yet aksdjh
 					fragment.eligible = false;
