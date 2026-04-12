d5ccd86d1cd003656b7d9c2b192c50afeed88dc9
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2026-04-11 23:27:59 -0600

    oops

--- data/mods/gen9evolutionproject/rulesets.ts
+++ data/mods/gen9evolutionproject/rulesets.ts
@@ -700,7 +700,7 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 			// report randomizer teams
 			for (const side of this.sides) {
 				let randomized = 0;
-				for (const const set of side.team) if (set && set.hasBeenRandomized) randomized++;
+				for (const set of side.team) if (set && set.hasBeenRandomized) randomized++;
 				if (randomized === side.team.length) {
 					this.add(`raw|${side.name}'s team was randomly generated!<br>If you want to use it again, you can copy it from here:`);
 					this.add('showteam', side.id, Teams.pack(side.team));
