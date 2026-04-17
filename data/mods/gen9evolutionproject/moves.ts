d5046670aa6486a65903c0708131be6d36e669c0
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2026-04-16 21:35:27 -0600

    Probably the best option

--- data/mods/gen9evolutionproject/moves.ts
+++ data/mods/gen9evolutionproject/moves.ts
@@ -1079,9 +1079,8 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 				if (pokemon.species.gayLizard) {
 					this.add('-ohko');
 					this.add('-sethp', pokemon, 0, '[from] move: Attract', '[silent]');
-					this.add('-anim', pokemon, "Memento", pokemon);
 					this.add('-message', `...`);
-					this.add('-anim', source, "Fake Tears", source);
+					this.add('-anim', source, "Amnesia", source);
 					this.add('-message', `...`);
 					this.add('-anim', pokemon, "Splash", pokemon);
 					this.add('-message', `... Okay he's fine`);
