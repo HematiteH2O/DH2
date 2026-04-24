c030dc50f6d7e20c5a2f53d1c84d770fc889b4ca
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2026-04-23 20:32:13 -0600

    Update scripts.ts

--- data/mods/gen9evolutionproject/scripts.ts
+++ data/mods/gen9evolutionproject/scripts.ts
@@ -1958,7 +1958,6 @@ singles ['choicebreaker', 'priority', 'entryhazard', 'hazardcontrol', 'knockoff'
 			}
 		}
 
-		let consoleLoggedEveryFragment = false;
 		while (eligibleFragments) {
 			// if there are already no fragments left on any Pokémon, immediately set eligibleFragments to false and then "continue;" to end the loop
 			if (!fragmentsList.length) {
@@ -2394,13 +2393,6 @@ singles ['choicebreaker', 'priority', 'entryhazard', 'hazardcontrol', 'knockoff'
 			}
 			if (weightedFragmentsListThisStep.length) fragmentsListThisStep = weightedFragmentsListThisStep;
 
-			if (!consoleLoggedEveryFragment) {
-				consoleLoggedEveryFragment = true;
-				console.log(`FRAGMENTS LIST`);
-				for (const fragment of fragmentsList) {
-					console.log(fragment);
-				}
-			}
 			// STEP 5: applying fragments
 			// finally, pick a random fragment from the narrowed-down pool, apply it to the set, and loop
 			let chosenFragment = this.sample(fragmentsListThisStep);
