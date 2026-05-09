608ae016d5412706beb5b79ec4a39ba42d9f62c9
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2026-05-08 23:26:08 -0600

    Perhaps

--- data/mods/gen9evolutionproject/scripts.ts
+++ data/mods/gen9evolutionproject/scripts.ts
@@ -16,7 +16,7 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 	
 	init() {
 		// for the experiment
-		const dexOfTheDay = Dex.mod('gen9evolutionproject');
+		const dexOfTheDay = Dex.dexes.gen9evolutionproject;
 		if (!(dexOfTheDay.data.Pokedex.bulbasaur.fakemonInitialized)) {
 			dexOfTheDay.data.Pokedex.bulbasaur.fakemonInitialized = true;
 		} else {
