d0f3ef9e1fa2bc59dce9117aca3b8799b66ce0c3
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2026-05-19 18:17:39 -0600

    okay

--- data/mods/gen9evolutionproject/scripts.ts
+++ data/mods/gen9evolutionproject/scripts.ts
@@ -3312,9 +3312,9 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 		
 		if (team.length) {
 			for (const pokemon of team) {
-				pokemon.requestedSupport = this.dex.data.Pokedex[pokemon.species].randbats[format].requestedSupport;
-				pokemon.offeredSupport = this.dex.data.Pokedex[pokemon.species].randbats.offeredSupport;
-				pokemon.acceptedSupport = this.dex.data.Pokedex[pokemon.species].randbats[format].acceptedSupport;
+				pokemon.offeredSupport = this.dex.species.get(pokemon.species).randbats.offeredSupport;
+				pokemon.requestedSupport = this.dex.species.get(pokemon.species).randbats[format].requestedSupport;
+				pokemon.acceptedSupport = this.dex.species.get(pokemon.species).randbats[format].acceptedSupport;
 				pokemon.coveredStabs = [];
 				pokemon.remainingStabTypes = [];
 				pokemon.remainingStabMoves = [];
