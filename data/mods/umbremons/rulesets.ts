527426b3f28b7dd3a422af37374add001a3ce589
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2026-07-03 17:55:36 -0600

    Should be good

--- data/mods/umbremons/rulesets.ts
+++ data/mods/umbremons/rulesets.ts
@@ -14,34 +14,20 @@
 				for (const pokemon of side.pokemon) {
 					let species = this.dex.species.get(pokemon.species.name);
 					
-					// add one more line between each Fakemon
-					if (species && (species.copyData || (species.evos && species.evos.length))) {
+					let modded = false;
+					const baseSpecies = Dex.species.get(pokemon.species.name);
+					for (const type in [0, 1]) if (species.types[type] !== baseSpecies.types[type]) modded = true;
+					for (const ability in [0, 1, 'H', 'S']) if (species.abilities[ability] !== baseSpecies.abilities[ability]) modded = true;
+					// we don't need a base stat check since Umbremons doesn't allow changes to those!
+					if (species.movepoolAdditions || species.movepoolDeletions) modded = true;
+					
+					// add one more line between each modded Pokémon
+					if (species && modded) {
+						showFakemon = true; // this lets us know that at least one modded Pokémon exists on the team; if not, we won't want to print anything!
+						
 						if (extraLineBreak) hideBox += `<br>`;
-						else extraLineBreak = true;
-					}
-
-					// report Eviolite compatibility even for canon Pokémon
-					if (species && !species.copyData && (species.evos && species.evos.length)) {
-						showFakemon = true;
-						hideBox += `<br><div class="hint">${species.name} <strong>can use Eviolite</strong> because it evolves into`;
-						let order = 0;
-						for (const evoname of species.evos) {
-							order++;
-							if (order < species.evos.length) {
-								hideBox += ` ${evoname}`;
-								if (order + 1 < species.evos.length) hideBox += `,`;
-							}
-							else {
-								if (species.evos.length !== 1) hideBox += ` and`;
-								hideBox += ` ${evoname}`;
-							}
-						}
-						hideBox += `!</div><br>`;
-					}
-
-					// otherwise, Fakemon
-					if (species && species.copyData) { // all modded things in Evo have this
-						showFakemon = true;
+						else extraLineBreak = true; // this gives us an extra linebreak before every Pokémon except the first
+						
 						let abilities = species.abilities[0];
 						if (species.abilities[1]) abilities += ` / ${species.abilities[1]}`;
 						if (species.abilities['H']) abilities += ` // ${species.abilities['H']}`;
@@ -52,10 +38,11 @@
 						hideBox += `</span></li><br><li class="result"><span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities + `</span><span class="col abilitycol"></span></span></li><br><li class="result"><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul></div>`;
 						
 						let customGuide = `<br><div class="infobox" open><details class ="details"><summary>More details on ${species.name}</summary>`;
-						
-						// creator
+
+						// these two sections are relevant to Evo 2, but probably not to Umbremons, so I'm commenting them out for now; feel free to add them back if you want!
+						/*
 						if (species.creator) {
-							customGuide += `<div class="hint"><br>${species.name} was created by ${species.creator}!</div>`;
+							customGuide += `<div class="hint"><br>${species.name} was modified by ${species.creator}!</div>`;
 						}
 
 						if (species.evos && species.evos.length) {
@@ -74,16 +61,12 @@
 							}
 							customGuide += `!</div>`;
 						}
+						*/
 						
-						// movepool changes
-						const gen9only = [
-							'Plankteenie', 'Mareanie-Drifter', 'Toxapex-Glacial', 'Nemesyst', 'Numel-Dormant', 'Dormedary', 'Dormaderupt',
-							'Uraxys', 'Cytoxys', 'Adexys', 'Guaxys', 'Riboxys-U', 'Riboxys-C', 'Riboxys-A', 'Riboxys-G',
-						];
-						customGuide += `<br><div class="hint">Its movepool is based on ${species.copyMoves ? species.copyMoves : species.copyData}'s`;
-						if (gen9only.includes(species.name)) customGuide += ` <strong>Gen IX</strong> movepool`;
+						// Movepool changes
+						// This section is slightly reworded from the Evo 2 version, since the modded Pokémon in Umbremons aren't Fakemon
 						if (species.movepoolAdditions) {
-							customGuide += `,<br>and it <strong>gained</strong> the move`;
+							customGuide += `<br><div class="hint">${species.name} <strong>gained</strong> the move`;
 							if (species.movepoolAdditions.length > 1) customGuide += `s`;
 							let order = 0;
 							for (const moveid of species.movepoolAdditions) {
@@ -100,7 +83,9 @@
 							}
 						}
 						if (species.movepoolDeletions) {
-							customGuide += `,<br>but it <strong>lost</strong> the move`;
+							if (species.movepoolAdditions) customGuide += `,<br>but it <strong>lost</strong> the move`;
+							else customGuide += `<br><div class="hint">${species.name} <strong>lost</strong> the move`;
+							
 							if (species.movepoolDeletions.length > 1) customGuide += `s`;
 							let order = 0;
 							for (const moveid of species.movepoolDeletions) {
@@ -116,55 +101,15 @@
 								}
 							}
 						}
-						if (!species.movepoolAdditions && !species.movepoolDeletions) customGuide += ` with no changes`;
-						customGuide += `.</div>`;
+						if (species.movepoolAdditions || species.movepoolDeletions) customGuide += `.</div>`;
 						
 						// custom Abilities
-						if (species.abilities[0]) {
-							let ability = this.dex.abilities.get(species.abilities[0]);
-							if (ability.num && ability.num < 0) { // report custom Abilities only
+						for (const num in [0, 1, 'H', 'S']) if (species.abilities[num]) {
+							let ability = this.dex.abilities.get(species.abilities[num]);
+							if (ability && ability.modded) { // report custom Abilities only
 								customGuide += `<br><div class="message"><li class="result"><span class="col namecol"><strong>${ability.name}</strong></span>`;
-								if (ability.longDesc) {
-										customGuide += `<br><font color="#686868">${ability.longDesc}</font>`;
-								} else if (ability.shortDesc) {
-										customGuide += `<br><font color="#686868">${ability.shortDesc}</font>`;
-								}
-								customGuide += `</li></div>`;
-							}
-						}
-						if (species.abilities[1]) {
-							let ability = this.dex.abilities.get(species.abilities[1]);
-							if (ability.num && ability.num < 0) { // report custom Abilities only
-								customGuide += `<br><div class="message"><li class="result"><span class="col namecol"><strong>${ability.name}</strong></span>`;
-								if (ability.longDesc) {
-										customGuide += `<br><font color="#686868">${ability.longDesc}</font>`;
-								} else if (ability.shortDesc) {
-										customGuide += `<br><font color="#686868">${ability.shortDesc}</font>`;
-								}
-								customGuide += `</li></div>`;
-							}
-						}
-						if (species.abilities['H']) {
-							let ability = this.dex.abilities.get(species.abilities['H']);
-							if (ability.num && ability.num < 0) { // report custom Abilities only
-								customGuide += `<br><div class="message"><li class="result"><span class="col namecol"><strong>${ability.name}</strong></span>`;
-								if (ability.longDesc) {
-										customGuide += `<br><font color="#686868">${ability.longDesc}</font>`;
-								} else if (ability.shortDesc) {
-										customGuide += `<br><font color="#686868">${ability.shortDesc}</font>`;
-								}
-								customGuide += `</li></div>`;
-							}
-						}
-						if (species.abilities['S']) {
-							let ability = this.dex.abilities.get(species.abilities['S']);
-							if (ability.num && ability.num < 0) { // report custom Abilities only
-								customGuide += `<br><div class="message"><li class="result"><span class="col namecol"><strong>${ability.name}</strong></span>`;
-								if (ability.longDesc) {
-										customGuide += `<br><font color="#686868">${ability.longDesc}</font>`;
-								} else if (ability.shortDesc) {
-										customGuide += `<br><font color="#686868">${ability.shortDesc}</font>`;
-								}
+								if (ability.longDesc) customGuide += `<br><font color="#686868">${ability.longDesc}</font>`;
+								else if (ability.shortDesc) customGuide += `<br><font color="#686868">${ability.shortDesc}</font>`;
 								customGuide += `</li></div>`;
 							}
 						}
@@ -173,17 +118,14 @@
 						if (species.movepoolAdditions) {
 							for (const moveid of species.movepoolAdditions) {
 								let move = this.dex.moves.get(moveid);
-								if (move.num < 0) { // report custom moves only
+								if (move && move.modded) { // report custom moves only
 									let power = move.basePower;
 									if (power < 2) power = "—";
 									let acc = move.accuracy;
 									if (acc === true) acc = "—";
 									customGuide += `<br><div class="message"><ul class="utilichart"><li class="result"><span class="col movenamecol"><strong>${move.name}</strong></span><span class="col typecol"><img src="https://play.pokemonshowdown.com/sprites/types/${move.type}.png" alt="${move.type} width="32" height="14"><img src="https://play.pokemonshowdown.com/sprites/categories/${move.category}.png" alt="${move.category}" width="32" height="14"></span><span class="col labelcol"><em>Power</em><br>${power}</span><span class="col widelabelcol"><em>Accuracy</em><br>${acc}</span><span class="col pplabelcol"><em>PP</em><br>${Math.floor(move.pp * 8 / 5)}</span></li></ul></div>`;
-									if (move.longDesc) {
-										customGuide += `<br><font color="#686868">${move.longDesc}</font>`;
-									} else if (move.shortDesc) {
-										customGuide += `<br><font color="#686868">${move.shortDesc}</font>`;
-									}
+									if (move.longDesc) customGuide += `<br><font color="#686868">${move.longDesc}</font>`;
+									else if (move.shortDesc) customGuide += `<br><font color="#686868">${move.shortDesc}</font>`;
 								}
 							}
 						}
@@ -199,50 +141,25 @@
 				if (showFakemon) this.add(`${hideBox}`);
 			}
 		},
-		onDataMod(pokemon) {
-			let species = this.dex.species.get(pokemon.species.name);
-			if (species.copyData) { // all modded things in Evo have this
-				this.add('-start', pokemon, 'typechange', pokemon.getTypes(true).join('/'), '[silent]');
-				let abilities = species.abilities[0];
-				if (species.abilities[1]) abilities += ` / ${species.abilities[1]}`;
-				if (species.abilities['H']) abilities += ` // ${species.abilities['H']}`;
-				if (species.abilities['S']) abilities += ` // <i>(${species.abilities['S']})</i>`;
-				const baseStats = species.baseStats;
-				const type = species.types[0];
-				if (species.types[1]) {
-					const type2 = species.types[1];
-					this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="http://play.pokemonshowdown.com/sprites/types/${type}.png" alt="${type}" height="14" width="32"><img src="http://play.pokemonshowdown.com/sprites/types/${type2}.png" alt="${type2}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities + `</span><span class="col abilitycol"></span></span><br><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
-				} else {
-					this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="http://play.pokemonshowdown.com/sprites/types/${type}.png" alt="${type}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities + `</span><span class="col abilitycol"></span></span><br><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
-				}
-				if (species.creator) this.hint(`${species.name} was created by ${species.creator}!`);
-				// might add movepool additions?
-				// this.hint(`text goes here.`, true);
-			}
-		},
 		onSwitchIn(pokemon) {
-			if (pokemon.illusion) {
-				this.add('-start', pokemon, 'typechange', pokemon.illusion.getTypes(true).join('/'), '[silent]');
-			} else {
-				this.add('-start', pokemon, 'typechange', pokemon.getTypes(true).join('/'), '[silent]');
-			}
+			this.add('-start', pokemon, 'typechange', (pokemon.illusion ? pokemon.illusion.getTypes(true) : pokemon.getTypes(true)).join('/'), '[silent]');
 		},
 		onDamagingHit(damage, target, source, move) {
 			if (target.hasAbility('illusion')) { // making sure the correct information is given when an Illusion breaks
-				if (this.dex.species.get(target.species.name).copyData) { // if the target is modded
-					this.add('-start', target, 'typechange', target.getTypes(true).join('/'), '[silent]');
-				} else {
+				// displaying type changes
+				let species = this.dex.species.get(target.species.name);
+				const baseSpecies = Dex.species.get(target.species.name);
+				
+				let modded = false;
+				for (const type in [0, 1]) if (species.types[type] !== baseSpecies.types[type]) modded = true;
+				
+				if (modded) this.add('-start', target, 'typechange', target.getTypes(true).join('/'), '[silent]');
+				else {
 					const types = target.baseSpecies.types;
 					if (target.getTypes().join() === types.join()) {
 						this.add('-end', target, 'typechange', '[silent]');
 					}
 				}
-			}
-			// super-effective move count
-			if (!move.damage && !move.damageCallback && target.getMoveHitData(move).typeMod > 0) {
-				if (source.m.superEffectiveHits) {
-					source.m.superEffectiveHits++;
-				} else source.m.superEffectiveHits = 1;
 			}
 		},
 	},
